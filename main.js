575c7019e47c988660935ffc5a25a6b35b1d32ca
LuisEscamilla9708 <127147479+LuisEscamilla9708@users.noreply.github.com>
2025-03-21 00:37:58 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 onload = () => {
         // 1. Elimina la clase "container" para activar animaciones
         document.body.classList.remove("container");
-    
+
+document.addEventListener("DOMContentLoaded", function() {
+    let nameContainer = document.querySelector(".name");
+    let text = ["A", "N", "A", "H", "I"];
+
+    nameContainer.innerHTML = ""; // Borra cualquier traducción previa
+
+    text.forEach(letter => {
+      let span = document.createElement("span");
+      span.classList.add("letter");
+      span.textContent = letter;
+      nameContainer.appendChild(span);
+
+        
         // 2. Activa la música en caso de que el autoplay sea bloqueado
         const audio = document.getElementById('bg-music');
         window.addEventListener('click', () => {
